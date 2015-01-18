11b55cede7814b14f834c408653a3c711b908fe9
joromir <h8guitar@gmail.com>
2015-01-18 15:31:00 +0200

    new solution for hw3

--- solutions/03.rb
+++ solutions/03.rb
@@ -1,154 +1,96 @@
 module RBFS
-  module Helper
-    def Helper.string_number(string)
-      string.include?(".") ? string.to_f : string.to_i
+  class Parser
+    def initialize(string_data)
+      @string_data = string_data
     end
 
-    def Helper.generate_files(string_data)
-      file_count = string_data.to_i
-      files = []
-      data = string_data[file_count.to_s.length + 1 ... string_data.length]
-      0.upto(file_count - 1) do
-        file_info = read_file(data)
-        files << file_info[0..1]
-        data = file_info[2]
+    def parse_list
+      objects_count, @string_data = @string_data.split(':', 2)
+      objects_count.to_i.times do
+        name, length, rest = @string_data.split(':', 3)
+        yield name, rest[0...length.to_i]
+        @string_data = rest[length.to_i..-1]
       end
-
-      [files, data]
-    end
-
-    def Helper.generate_directories(string_data)
-      dir_count = string_data.to_i
-      string_directories = []
-      while dir_count > 0 do
-        info = read_directory(string_data[dir_count.to_s.length + 1...string_data.size])
-        string_directories << info[0..1]
-        string_data = info[2]
-        dir_count = dir_count - 1
-      end
-
-      string_directories
-    end
-
-    def Helper.read_file(data)
-      name = data.partition(":")[0]
-      length = data[name.size + 1 ...data.length].to_i
-      left = name.to_s.length + length.to_s.length + 2
-      right = name.to_s.length + length.to_s.length + length + 2
-      content = data[left...right]
-
-      [name, RBFS::File.parse(content),
-       data[name.to_s.length + length.to_s.length + length + 2...data.size]]
-    end
-
-    def Helper.read_directory(string_data)
-      name = string_data.partition(":")[0]
-      length = string_data[name.to_s.length + 1...string_data.size].to_i
-      left = name.to_s.length + length.to_s.size + 2
-      right = name.to_s.length + length.to_s.size + length
-      content = string_data[left...right]
-
-      [name,
-       content,
-       string_data[name.to_s.length + length.to_s.size + length...string_data.size]]
     end
   end
 
   class File
-    include Helper
-    attr_reader :data_type
     attr_accessor :data
 
-    def initialize(file = nil)
-      @data = file
-      @data_type = determine_type
+    def initialize(data = nil)
+      @data = data
     end
 
-    def determine_type
+    def data_type
       case @data
-      when String     then :string
-      when Symbol     then :symbol
-      when Numeric    then :number
-      when TrueClass  then :boolean
-      when FalseClass then :boolean
-      when NilClass   then :nil
+        when String                then :string
+        when Symbol                then :symbol
+        when Fixnum, Float         then :number
+        when TrueClass, FalseClass then :boolean
+        when NilClass              then :nil
       end
     end
 
     def serialize
-      @data_type = determine_type
-      "#@data_type:#@data"
+      "#{data_type}:#{data}"
     end
 
-    def size
-      serialize.length
-    end
-
-    def self.parse(serialized)
-      file = case serialized.partition(":")[0].to_sym
-             when :string  then serialized[7..serialized.length]
-             when :symbol  then serialized[7..serialized.length].to_sym
-             when :number  then Helper.string_number(serialized[7..serialized.length])
-             when :boolean then serialized[8..serialized.length] == "true"
-             when :nil     then nil
+    def self.parse(string_data)
+      data_type, data = string_data.split(':', 2)
+      data = case data_type
+               when 'string'  then data
+               when 'symbol'  then data.to_sym
+               when 'number'  then data.to_f
+               when 'boolean' then data == 'true'
              end
-
-      RBFS::File.new(file)
+      File.new(data)
     end
   end
 
   class Directory
-    include Helper
-    attr_reader :files
-    attr_reader :directories
+    attr_reader :files, :directories
 
-    def initialize(directories = {})
+    def self.parse(string_data)
+      directory = Directory.new
+      parser = Parser.new(string_data)
+      parser.parse_list do |name, data|
+        directory.add_file(name, File.parse(data))
+      end
+      parser.parse_list do |name, data|
+        directory.add_directory(name, Directory.parse(data))
+      end
+      directory
+    end
+
+    def initialize
       @files = {}
-      @directories = directories
+      @directories = {}
     end
 
     def add_file(name, file)
-      @files[name] = file unless name.include?(":")
+      @files[name] = file
     end
 
-    def add_directory(name, directory = RBFS::Directory.new)
-      corectness = name.include?(":")
-      @directories[name] = directory unless corectness
+    def add_directory(name, directory = Directory.new)
+      @directories[name] = directory
     end
 
     def [](name)
-      if @directories.include?(name)
-        @directories[name]
-      elsif @files.include?(name)
-        file = RBFS::File.new
-        file.data = @files[name].data
-        file.determine_type
-        file
-      end
+      @directories[name] || @files[name]
     end
 
     def serialize
-      files = @files.to_a
-                    .map { |n, f| n + ":" + f.size.to_s + ":" + f.serialize }
-                    .reduce("") { |a, b| a + b }
-      dirs = @directories.to_a
-                         .map { |n, d| n + ":" + d.size.to_s + ":" + d.serialize }
-                         .reduce("") { |a, b| a + b }
-
-      "#{@files.count}:#{files}#{@directories.count}:#{dirs}"
+      "#{serialize_list(@files)}#{serialize_list(@directories)}"
     end
 
-    def size
-      serialize.length
-    end
+    private
 
-    def self.parse(string_data)
-      result = RBFS::Directory.new
-      files = Helper.generate_files(string_data)
-      files[0].each { |name, file| result.add_file(name, file) }
-      string_directories = Helper.generate_directories(files[1])
-      string_directories.each { |name, dir| result.add_directory(name, parse(dir)) }
-      result
+    def serialize_list(objects)
+      serialized_objects = objects.map do |name, object|
+        serialized_object = object.serialize
+        "#{name}:#{serialized_object.length}:#{serialized_object}"
+      end
+      "#{objects.count}:#{serialized_objects.join('')}"
     end
   end
 end
